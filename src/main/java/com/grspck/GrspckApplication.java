75750becb22c53585145be6b8f44d316993b762e
mmadry14 <73359933+mmadry14@users.noreply.github.com>
2021-01-05 11:03:15 +0100

    taktyczne sprawdzenie

--- src/main/java/com/grspck/GrspckApplication.java
+++ src/main/java/com/grspck/GrspckApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GrspckApplication {
-
+//sprawdzenie
     public static void main(String[] args) {
         SpringApplication.run(GrspckApplication.class, args);
     }
